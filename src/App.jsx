7c3cb313622a84775e4f36e7b137a52477496b58
chunogreg <chunogreg@yahoo.com>
2023-10-25 02:14:51 -0700

    some changes in the sound url

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,51 @@ import { useEffect, useState } from "react";
 
 function App() {
   const drums = [
-    { letter: "Q", id: "heater-1", src: "src/components/Heater1.mp3" },
-    { letter: "W", id: "heater-2", src: "src/components/Heater-2.mp3" },
-    { letter: "E", id: "heater-3", src: "src/components/Heater-3.mp3" },
-    { letter: "A", id: "heater-4", src: "src/components/Heater-4_1.mp3" },
-    { letter: "S", id: "open-hh", src: "src/components/Cev_H2.mp3" },
-    { letter: "D", id: "kick-n-hat", src: "src/components/Kick_n_Hat.mp3" },
-    { letter: "Z", id: "dsc-oh", src: "src/components/Dsc_Oh.mp3" },
-    { letter: "X", id: "kick", src: "src/components/Heater-6.mp3" },
-    { letter: "C", id: "rp4-kick", src: "src/components/RP4_KICK_1.mp3" },
+    {
+      letter: "Q",
+      id: "heater-1",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      letter: "W",
+      id: "heater-2",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      letter: "E",
+      id: "heater-3",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      letter: "A",
+      id: "heater-4",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      letter: "S",
+      id: "open-hh",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      letter: "D",
+      id: "kick-n-hat",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      letter: "Z",
+      id: "dsc-oh",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      letter: "X",
+      id: "kick",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      letter: "C",
+      id: "rp4-kick",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
   ];
 
   const [displaykey, setDisplaykey] = useState("");
